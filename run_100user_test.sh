a81cf3ada8b8dccc2527db6b022f763ff67fd512
AgamAulakh <aulakhagam@gmail.com>
2024-03-18 09:11:25 +0000

    fix duration to NOT be in ms

--- run_100user_test.sh
+++ run_100user_test.sh
@@ -17,13 +17,16 @@ else
     echo "Instance flag provided: $instances"
 
     echo "Starting auth test with 100 users with $instances instances"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -l results/horizontal/trial"${trial}"_inst"$instances"_auth_100users.csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 600 -l results/horizontal/trial${trial}_inst{$instances}_auth_100users.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 600 -l results/horizontal/trial"${trial}"_inst"{$instances}"_auth_100users.csv -n
 
     echo "Starting web heavy test with 100 users with $instances instances"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -l results/horizontal/trial"{$trial}"_inst"$instances"_webhvy_100users.csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 600 -l results/horizontal/trial{$trial}_inst{$instances}_webhvy_100users.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 600 -l results/horizontal/trial"{$trial}"_inst"{$instances}"_webhvy_100users.csv -n
 
     echo "Starting web light test with 100 users with $instances instances"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -l results/horizontal/trial"{$trial}"_inst"$instances"_weblgt_100users.csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -jdurationMs 600 -l results/horizontal/trial{$trial}_inst{$instances}_weblgt_100users.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 600 -l results/horizontal/trial"{$trial}"_inst"{$instances}"_weblgt_100users.csv -n
 
 fi
 
--- run_burstload_test.sh
+++ run_burstload_test.sh
@@ -16,30 +16,36 @@ fi
 for ((i=0; i<5; i++))
 do
     echo "Iteration $((i+1)): Starting auth test with 10 users"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60000 -l results/burstload/trial"{$trial}"_auth_10users_itr"$i".csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial{$trial}_auth_10users_itr$i.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial"{$trial}"_auth_10users_itr"$i".csv -n
 
 
     echo "Iteration $((i+1)): Starting auth test with 100 users"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60000 -l results/burstload/trial"{$trial}"_auth_100users_itr"$i".csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial{$trial}_auth_100users_itr$i.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial"{$trial}"_auth_100users_itr"$i".csv -n
 done
 
 for ((i=0; i<5; i++))
 do
     echo "Iteration $((i+1)): Starting web heavy test with 10 users"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60000 -l results/burstload/trial"{$trial}"_webhvy_10users_itr"$i".csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial{$trial}_webhvy_10users_itr$i.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial"{$trial}"_webhvy_10users_itr"$i".csv -n
 
 
     echo "Iteration $((i+1)): Starting web heavy test with 100 users"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60000 -l results/burstload/trial"{$trial}"_webhvy_100users_itr"$i".csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial{$trial}_webhvy_100users_itr$i.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial"{$trial}"_webhvy_100users_itr"$i".csv -n
 done
 
 for ((i=0; i<5; i++))
 do
     echo "Iteration $((i+1)): Starting web light test with 10 users"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60000 -l results/burstload/trial"{$trial}"_weblgt_10users_itr"$i".csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial{$trial}_weblgt_10users_itr$i.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 10 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial"{$trial}"_weblgt_10users_itr"$i".csv -n
 
 
     echo "Iteration $((i+1)): Starting web light test with 100 users"
-    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60000 -l results/burstload/trial"{$trial}"_weblgt_100users_itr"$i".csv -n
+    echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial{$trial}_weblgt_100users_itr$i.csv -n"
+    java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser 100 -JrampUp 0 -JdurationMs 60 -l results/burstload/trial"{$trial}"_weblgt_100users_itr"$i".csv -n
 done
 
