bc3fad9838d93f74a05ff6d8f6c93f5bdf2ce676
AgamAulakh <aulakhagam@gmail.com>
2024-04-21 22:34:47 -0600

    introduces bash scripts for jmeter to main ahh

---
+++ run_exp1_constload_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# we are testing 3 different groups of users for 10 mins each
+
+# python3 Monitoring\ scripts/OSMonitor/osmonitor.py --out-dir results/constload/ --test-name trial"${trial}"_allusers
+
+trial="$1"
+
+# Check if the trial flag value is provided
+if [ -z "$trial" ]; then
+    echo "Trial Flag not provided."
+else
+    echo "Trial Flag provided: $trial"
+    rm -f results/constload/trial"${trial}"*
+
+    user_list=(10 50 100)
+    for user in "${user_list[@]}"
+    do
+	echo "Starting auth test for ${user} users"
+	echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser ${user} -JrampUp 0 -JdurationMs 600 -l results/constload/trial${trial}_auth_${user}users.csv -n"
+	java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_authentication.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser "${user}" -JrampUp 0 -JdurationMs 600 -l results/constload/trial"${trial}"_auth_"${user}"users.csv -n
+
+	echo "Starting web heavy test for ${user} users"
+	echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser ${user} -JrampUp 0 -JdurationMs 600 -l results/constload/trial${trial}_webhvy_${user}users.csv -n"
+	java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_heavy.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser "${user}" -JrampUp 0 -JdurationMs 600 -l results/constload/trial"${trial}"_webhvy_"${user}"users.csv -n
+
+	echo "Starting web light test for ${user} users"
+	echo "java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser ${user} -JrampUp 0 -JdurationMs 600 -l results/constload/trial${trial}_weblgt_${user}users.csv -n"
+	java -jar ~/apache-jmeter-5.4.3/bin/ApacheJMeter.jar -t goal-1/teastore_browse_nogui_webui_light.jmx -Jhostname 127.0.0.1 -Jport 8080 -JnumUser "${user}" -JrampUp 0 -JdurationMs 600 -l results/constload/trial"${trial}"_weblgt_"${user}"users.csv -n
+    done
+fi
+
